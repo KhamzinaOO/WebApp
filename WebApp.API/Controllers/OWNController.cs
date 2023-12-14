fc99f7dd8f4051c23c67b0723c4982b05c478715
Olga <Olga@DESKTOP-HI8ST33>
2023-12-14 22:19:09 +0300

    Добавьте файлы проекта.

---
+++ WebApp.API/Controllers/OWNController.cs
@@ -0,0 +1,17 @@
+﻿using PidzhaWEB;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Infrastructure;
+
+namespace PidzhaWEB.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OWNController : ControllerBase
+    {
+
+
+        private readonly Context _context;
+
+    }
+}
